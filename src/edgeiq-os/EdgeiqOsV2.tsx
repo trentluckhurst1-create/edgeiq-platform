f6bf53eb8489b817542ea4c268136ab531f40e7b
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-12 07:39:11 +1000

    Load Form workspace styles

--- src/edgeiq-os/EdgeiqOsV2.tsx
+++ src/edgeiq-os/EdgeiqOsV2.tsx
@@ -3,6 +3,7 @@ import "./styles/edgeiqDashboardLocked.css";
 import "./styles/edgeiqMeetingsLocked.css";
 import "./styles/edgeiqRaceV1.css";
 import "./styles/edgeiqFieldV1.css";
+import "./styles/edgeiqFormV1.css";
 import "./styles/edgeiqLightSidebarLock.css";
 import { EdgeiqOsShell } from "./shell/EdgeiqOsShell";
 
