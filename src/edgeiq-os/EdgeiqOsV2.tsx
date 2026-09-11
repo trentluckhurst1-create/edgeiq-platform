ad4fd212e6f4ce59081c31197788d7c26c8d8f69
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-12 05:46:49 +1000

    Remove competing visual patches and load one hard visual contract

--- src/edgeiq-os/EdgeiqOsV2.tsx
+++ src/edgeiq-os/EdgeiqOsV2.tsx
@@ -1,14 +1,8 @@
 import "./styles/edgeiqOsV2.css";
 import "./styles/edgeiqDashboardLocked.css";
 import "./styles/edgeiqMeetingsLocked.css";
-import "./styles/edgeiqRaceLocked.css";
-import "./styles/edgeiqRaceConsistencyLock.css";
-import "./styles/edgeiqRaceConsistencyPatch.css";
 import "./styles/edgeiqLightSidebarLock.css";
-import "./styles/edgeiqAllRaceTabsLocked.css";
-import "./styles/edgeiqRaceFieldFormFinal.css";
-import "./styles/edgeiqUniversalWorkspaceFinal.css";
-import "./styles/edgeiqUniversalSubtabsFinal.css";
+import "./styles/edgeiqHardVisualReset.css";
 import { EdgeiqOsShell } from "./shell/EdgeiqOsShell";
 
 export function EdgeiqOsV2() {
