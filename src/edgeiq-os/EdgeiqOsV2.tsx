3f1e1ccc3c20b5a8d8a897bd6ecd68023af8c4e9
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-12 05:33:47 +1000

    Load universal workspace visual contract last

--- src/edgeiq-os/EdgeiqOsV2.tsx
+++ src/edgeiq-os/EdgeiqOsV2.tsx
@@ -7,6 +7,7 @@ import "./styles/edgeiqRaceConsistencyPatch.css";
 import "./styles/edgeiqLightSidebarLock.css";
 import "./styles/edgeiqAllRaceTabsLocked.css";
 import "./styles/edgeiqRaceFieldFormFinal.css";
+import "./styles/edgeiqUniversalWorkspaceFinal.css";
 import { EdgeiqOsShell } from "./shell/EdgeiqOsShell";
 
 export function EdgeiqOsV2() {
