e3b59f71cf7de8fc43d5dfb56dd21af5e2271fb3
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-11 14:41:15 +1000

    Load light sidebar hard lock after legacy EDGEiQ CSS

--- src/edgeiq-os/EdgeiqOsV2.tsx
+++ src/edgeiq-os/EdgeiqOsV2.tsx
@@ -1,5 +1,6 @@
 import "./styles/edgeiqOsV2.css";
 import "./styles/edgeiqDashboardLocked.css";
+import "./styles/edgeiqLightSidebarLock.css";
 import { EdgeiqOsShell } from "./shell/EdgeiqOsShell";
 
 export function EdgeiqOsV2() {
