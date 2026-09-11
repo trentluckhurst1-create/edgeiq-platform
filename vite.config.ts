9190c96812617ae1c6e217da82e9b8ae61b7035b
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-11 13:44:01 +1000

    Fix GitHub Pages Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isGitHubActions ? "/edgeiq-platform/" : "/",
   plugins: [react()],
   server: {
     watch: {
