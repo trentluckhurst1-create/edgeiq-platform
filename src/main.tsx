97ab50280eeefa76abd40a0f8acc281165caa2f8
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-11 13:54:02 +1000

    Load EDGEiQ module normalisation styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./styles/edgeiqDesignSystem.css";
 import "./edgeiq-os/design-system/edgeiqDesignSystem.css";
 import "./edgeiq-os/approved-ui/edgeiqApprovedUiRebuildV1.css";
 import "./edgeiq-os/styles/edgeiqSoftwareSystem.css";
+import "./edgeiq-os/styles/edgeiqSoftwareModules.css";
 
 const env = (import.meta as ImportMeta & { env: Record<string, string | undefined> }).env;
 const publishableKey = env.VITE_CLERK_PUBLISHABLE_KEY;
