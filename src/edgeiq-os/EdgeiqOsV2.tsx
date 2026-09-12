e88c0ffa939847d4409c79ff5702a05a80d9d553
Trent Luckhurst <trent.luckhurst1@gmail.com>
2026-09-12 11:19:22 +1000

    Load clean Overview workspace styling

--- src/edgeiq-os/EdgeiqOsV2.tsx
+++ src/edgeiq-os/EdgeiqOsV2.tsx
@@ -8,6 +8,7 @@ import "./styles/edgeiqPerformanceV1.css";
 import "./styles/edgeiqEpiRatingsV1.css";
 import "./styles/edgeiqSpeedMapV1.css";
 import "./styles/edgeiqMarketCleanV1.css";
+import "./styles/edgeiqOverviewV1.css";
 import "./styles/edgeiqLightSidebarLock.css";
 import { EdgeiqOsShell } from "./shell/EdgeiqOsShell";
 
